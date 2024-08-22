c9ef363722f174a86cd3aa2f5754552f23308670
Torin Etheridge <torinriley220@gmail.com>
2024-08-22 01:29:53 -0400

    Update acon__init__.py

--- acon/acon__init__.py
+++ acon/acon__init__.py
@@ -1,6 +1,7 @@
 from .data_mapping import AdaptiveDataMapper
 from .correlation_modules import CorrelationModule
 from .optimization import AdaptiveOptimizer
+from .adaptive_neural_architecture_search import ANAS
 from .loss_function import AdaptiveLossFunction
 from .adaptation import ContextualAdapter
 from .meta_learning import MetaLearner
