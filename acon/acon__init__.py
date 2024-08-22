6749ed7e55e0f590af5f1ab8c29b02d7cd81e980
Torin Etheridge <torinriley220@gmail.com>
2024-08-22 11:55:57 -0400

    Update acon__init__.py

--- acon/acon__init__.py
+++ acon/acon__init__.py
@@ -6,14 +6,16 @@ from .loss_function import AdaptiveLossFunction
 from .adaptation import ContextualAdapter
 from .meta_learning import MetaLearner
 from .real_time_integration import RealTimeDataIntegrator
+from .adaptive_hyperparameter_tuner import AdaptiveHyperparameterTuner  # New module added
 
 __all__ = [
     'AdaptiveDataMapper',
     'CorrelationModule',
-    'adaptive_neural_architecture_search'
     'AdaptiveOptimizer',
+    'ANAS',  # Corrected the reference to ANAS
     'AdaptiveLossFunction',
     'ContextualAdapter',
     'MetaLearner',
-    'RealTimeDataIntegrator'
+    'RealTimeDataIntegrator',
+    'AdaptiveHyperparameterTuner'  # Ensure the new module is listed
 ]
